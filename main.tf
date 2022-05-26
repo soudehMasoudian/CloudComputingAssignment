0129575bb3b89d2b2b0956303fde58e5f51f10fa
soudehMasoudian <soudeh.masoudian@fmail.com>
2022-05-26 16:20:38 +0430

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,30 +7,6 @@ provider "aws" {
   token = "FwoGZXIvYXdzEMv//////////wEaDJvRuxno/m5WuQn6YiLVAYQR4Zz5ChrVFzEk27Jip7BKxJpBd20VMwAxm7xdBf+QCvuG5g9y+OvV5p9APgkjvi6Sk8MKzdvhChkLc5e5Zlz2eZGrNnSgH+V9UhNWFJ759uAt6KymhOug7DxGN/X0FGjmOrN2DFesysUqM8gkIETIE9MVWbTV8Ghp3Q/b2JJOEoZXyQYRE/QhVyD1qnA/jgUpD9waXrlMLoFSOEfwW+ssR3e5KuVxsJzQVlbrtC+a8dZmLQ5npq78d7JPp7bVc1uH0CO5hqST+gV+y+YaNVTyS0eDTCinmr2UBjItgBDQi+SASlEyy4WIktUCdRwNv8DU8ykZJrI7NOuzGiW6CU7d6GlGOesY3cOP"
 }
 
-# module "cc2022-client2-web-app" {
-#     source = "./modules/s3website"
-#     region = local.region
-#     site_name = local.client_app_site_name
-# }
-
-# resource "aws_s3_object" "object" {
-#   bucket = local.client_app_site_name
-#   key = "test.html"
-#   source = "./test/html"
-#   content_type = "text/html"
-
-#   etag = filemd5("./test.html")
-# }
-
-# resource "aws_s3_object" "object2" {
-#   bucket = local.client_app_site_name
-#   key = "index.html"
-#   source = "./index.html"
-#   content_type = "text/html"
-
-#   etag = filemd5("./index.html")
-# }
-
 resource "aws_s3_bucket" "main_s3_bucket" {
   bucket = "soudeh-bucket-assignment"
   tags = {
